072fde8db33215f5c5ca0a2680857f9fe3865e41
bogus <bogusshtudy@gmail.com>
2025-03-01 17:38:33 +0530

    hellooo

---
+++ run.tf
@@ -0,0 +1,7 @@
+output "first_output" {
+  value = var.val1
+}
+
+output "second_output" {
+  value = var.val2
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "val1" {
+    default = 100
+    type = number
+}
+
+variable "val2" {
+    default = [10, 20, 30]
+    type = list
+}
